e742b8865365499dce57647d841eae04f614246d
Victor Glebov <glebovv3105@gmail.com>
2020-11-01 12:48:18 -0700

    Modified admin

--- table/admin.py
+++ table/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import TableField, CSVPath
 
-# Register your models here.
+
+@admin.register(TableField)
+class TableFieldAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CSVPath)
+class CSVPathAdmin(admin.ModelAdmin):
+    pass
