7bb636fc2eb589e1c333745eff1c29c6994ced2a
Victor Glebov <glebovv3105@gmail.com>
2020-11-01 12:29:35 -0700

    Added CSVPath model

--- table/models.py
+++ table/models.py
@@ -5,3 +5,14 @@ class TableField(models.Model):
     number = models.PositiveIntegerField(verbose_name='Порядковый номер')
     name = models.CharField(max_length=40, verbose_name='Имя')
     width = models.PositiveIntegerField(verbose_name='Ширина')
+
+
+class CSVPath(models.Model):
+    path = models.FilePathField()
+
+    def get_path(self):
+        return self.path
+
+    def set_path(self, path):
+        self.path = path
+        self.save()
